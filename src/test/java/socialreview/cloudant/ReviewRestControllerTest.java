5db579ce7739069ab8220b885b7accb0695c282b
Fabio Gomez <fabiogomez@us.ibm.com>
2016-12-09 15:47:03 -0600

    - Improved testing by leveraging the JSONObject notation when parsing response body for assertion testing

--- src/test/java/socialreview/cloudant/ReviewRestControllerTest.java
+++ src/test/java/socialreview/cloudant/ReviewRestControllerTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -12,7 +14,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import static io.restassured.RestAssured.when;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,8 +24,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-
-import static org.hamcrest.core.StringContains.containsString;
 
 
 @RunWith(SpringRunner.class)
@@ -38,6 +38,12 @@ public class ReviewRestControllerTest {
 
     // Random itemId to keep track of generated review
     final static private int itemId = 10000 + new Random().nextInt(90000);
+    // Rest of variables to for test review
+    final static private String comment = "Best product ever";
+    final static private int rating = 4;
+    final static private String reviewer_email = "someone@somedomain.com";
+    final static private String reviewer_name = "Some Person";
+    final static private String review_date = "12/8/2016";
 
     // Variable to hold the created review id
     static private String review_id;
@@ -59,12 +65,12 @@ public class ReviewRestControllerTest {
     public void test_02_create_review() {
         // Build test JSON
         Map<String, Object> jsonAsMap = new HashMap<>();
-        jsonAsMap.put("comment", "Best product ever");
+        jsonAsMap.put("comment", comment);
         jsonAsMap.put("itemId", itemId);
-        jsonAsMap.put("rating", 4);
-        jsonAsMap.put("reviewer_email", "someone@somedomain.com");
-        jsonAsMap.put("reviewer_name", "Some Person");
-        jsonAsMap.put("review_date", "12/8/2016");
+        jsonAsMap.put("rating", rating);
+        jsonAsMap.put("reviewer_email", reviewer_email);
+        jsonAsMap.put("reviewer_name", reviewer_name);
+        jsonAsMap.put("review_date", review_date);
 
         // Create Review
         Response response =
@@ -96,8 +102,24 @@ public class ReviewRestControllerTest {
                 response();
 
         // Find the review id in the JSON string
-        String json = response.asString();
-        assertThat(json, containsString(review_id));
+        String body_string = response.asString();
+
+        // Body is an array of JSON objects
+        JSONArray array = new JSONArray(body_string);
+
+        // Confirm length is just one since previously created only one review for a unique itemId
+        assertEquals(1, array.length());
+
+        // Confirm the one review is the one we created
+        JSONObject rev = array.getJSONObject(0);
+
+        assertEquals(review_id, rev.getString("_Id"));
+        assertEquals(comment, rev.getString("comment"));
+        assertEquals(itemId, rev.getInt("itemId"));
+        assertEquals(rating, rev.getInt("rating"));
+        assertEquals(reviewer_email, rev.getString("reviewer_email"));
+        assertEquals(reviewer_name, rev.getString("reviewer_name"));
+        assertEquals(review_date, rev.getString("review_date"));
 
     }
 
